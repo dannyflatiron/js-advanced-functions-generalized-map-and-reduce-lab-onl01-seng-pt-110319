15d58fca278b823ccf32c1fd0445fcf17e0c7eb3
dannyflatiron <dannyflatiron@gmail.com>
2020-06-16 11:35:53 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Add your functions here
+
+let map = function(array, codeBlock) {
+    let m = []
+
+    for (let i = 0; i < array.length; i++) {
+        let d = array[i]
+        m.push(codeBlock(d))
+    }
+    return m 
+}
+
+let reduce = function(array, codeBlock, startingPoint) {
+    // reduce(a, b)
+    let sp = (!!startingPoint) ? startingPoint : array[0]
+    let i = (!!startingPoint) ? 0 : 1
+
+    let m 
+    for(; i < array.length; i++){
+        sp = codeBlock(array[i], sp)
+    }
+    return sp 
+}
